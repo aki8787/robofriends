c50be7e4924e019582e99d084914ce8706f7f7e4
Akmal Sulaymonov <akmalsulaymonov@gmail.com>
2022-03-07 17:06:11 +0500

    Scroll integrated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import {robots} from './robots'
+import Scroll from './Scroll';
+//import {robots} from './robots'
 import './App.css';
 
 class App extends Component{
@@ -9,9 +10,15 @@ class App extends Component{
     constructor(){
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users/')
+            .then(response =>  response.json())
+            .then(users => {this.setState({ robots: users})} );
     }
 
     onSearchChange = (event) => {
@@ -24,13 +31,20 @@ class App extends Component{
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         });
 
-        return (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={ this.onSearchChange } />
-                <CardList robots={filteredRobots} />
-            </div>
-        );
+        if(this.state.robots.length === 0){
+            return <h1>Loading</h1>
+        }
+        else{
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={ this.onSearchChange } />
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
+                </div>
+            );
+        }
 
     }
 
